ac4408e2a273561983dba3a9e0b14844c32647d9
Max Witten <pexcn97@gmail.com>
2018-03-22 18:15:22 +0800

    add ignore list

--- wechat.py
+++ wechat.py
@@ -13,10 +13,17 @@ reply_text = '''这条消息是自动回复的。
 Telegram: @pexcn
 E-mail: pexcn97@gmail.com'''
 
+"""
+weixin: 微信团队
+filehelper: 文件传输助手
+"""
+ignore_list = ['weixin', 'filehelper']
+
 # 回复好友消息
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING, PICTURE, RECORDING, VOICE, ATTACHMENT, VIDEO])
 def chat_reply(msg):
-    itchat.send(reply_text, msg['FromUserName'])
+    if msg['FromUserName'] not in ignore_list:
+        itchat.send(reply_text, msg['FromUserName'])
 
 # 回复群聊 @ 我的消息
 @itchat.msg_register(TEXT, isGroupChat=True)
@@ -34,6 +41,16 @@ def send_message_task(target, min_sec, max_sec):
         msg = 'Delay: ' + str(delay) + 's\n' + 'Time: ' + time_str
         itchat.send(msg, toUserName=target)
 
+# 添加忽略列表
+def add_ignore_list(who=None):
+    if who == None:
+        info = itchat.search_friends()
+        ignore_list.append(info['UserName'])
+    else:
+        info = itchat.search_friends(name=who)
+        for i in range(len(info)):
+            ignore_list.append(info[i]['UserName'])
+
 # 微信登录
 itchat.auto_login(hotReload=True, enableCmdQR=True)
 
@@ -41,6 +58,13 @@ itchat.auto_login(hotReload=True, enableCmdQR=True)
 task = threading.Thread(target=send_message_task, args=('filehelper', 300, 600))
 task.setDaemon(True)
 task.start()
+
+# 把部分联系人添加到忽略列表
+add_ignore_list()
+add_ignore_list('妈妈')
+add_ignore_list('陈家亮')
+
+print('ignore list: ' + str(ignore_list))
 
 # 阻塞线程，保持微信登录
 itchat.run(debug=False)
